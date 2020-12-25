dfd43a0c0bf8477a5b68051e7229f3bac4bcccf8
Daniel_Gómez_G <dgomez.informatica@gmail.com>
2020-12-25 20:23:06 -0300

    No se hizo mayores modificaciones, solo testeo de registros

--- docs/biblioteca.sql
+++ docs/biblioteca.sql
@@ -21,17 +21,24 @@ CREATE TABLE IF NOT EXISTS libro (
 
 -- Datos temporales de ejemplo
 INSERT INTO biblioteca.autor(nombre)
-	VALUES('Julian Melgosa y Michelson Borges'),('Alba Rosa Pastora Olivares'),('Isha'),('Ediciones Paulinas Verbo Divino');
+	VALUES('Seleccione...'),('Julian Melgosa y Michelson Borges'),('Alba Rosa Pastora Olivares'),('Isha'),
+    ('Ediciones Paulinas Verbo Divino');
 SELECT a.idautor AS "#", a.nombre FROM biblioteca.autor a;
+-- SET FOREIGN_KEY_CHECKS = 0; 
+-- TRUNCATE table autor; 
+-- SET FOREIGN_KEY_CHECKS = 1;
 
 INSERT INTO biblioteca.libro(isbn, nombre, leyenda, descripcion, cantidad_bodega, num_paginas, idautor, precio)
-	VALUES (10502,'El poder de la esperanza','Secretos del bienestar emocional','Practicar ejercicio fisico y tener...',20,96,1,5200);
+	VALUES (10502,'El poder de la esperanza','Secretos del bienestar emocional','Practicar ejercicio fisico y tener...',20,96,2,5200);
 INSERT INTO biblioteca.libro(isbn, nombre, leyenda, descripcion, cantidad_bodega, num_paginas, idautor, precio)
-	VALUES (2015,'Camino al exito','Demoliendo mis zonas estrechos','Muchas veces no preocupamos de...',100,80,2,2380);
+	VALUES (2015,'Camino al exito','Demoliendo mis zonas estrechos','Muchas veces no preocupamos de...',100,80,3,2380);
 INSERT INTO biblioteca.libro(isbn, nombre, leyenda, descripcion, cantidad_bodega, num_paginas, idautor, precio)
-	VALUES (978956,'Sobre las nubes','El sol esta siempre brillando...','La vida es una oferta increible...',5,173,3,1000);
+	VALUES (978956,'Sobre las nubes','El sol esta siempre brillando...','La vida es una oferta increible...',5,173,4,1000);
 INSERT INTO biblioteca.libro(isbn, nombre, leyenda, descripcion, cantidad_bodega, num_paginas, idautor, precio)
-	VALUES (777,'La Biblia','!Jesucristo ha resucitado¡','La historia de Dios',10,388,4,8760);
+	VALUES (777,'La Biblia','!Jesucristo ha resucitado¡','La historia de Dios',10,388,5,8760);
+ SET FOREIGN_KEY_CHECKS = 0; 
+ TRUNCATE table libro; 
+ SET FOREIGN_KEY_CHECKS = 1;
 
 SELECT l.idlibro AS "#", l.isbn, l.nombre AS "titulo", l.leyenda, l.descripcion, l.cantidad_bodega AS "stock", 
 	l.num_paginas AS "paginas", a.nombre AS "author", l.precio
